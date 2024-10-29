6ba5b94b6127a630be1064e7498747dac2ae1d1e
OnetRares <onetrares@yahoo.com>
2024-10-29 13:39:52 +0200

    Test

--- ModernProject/Player.h
+++ ModernProject/Player.h
@@ -8,9 +8,13 @@ public:
 	Player( std::string_view name,std::pair<uint16_t, uint16_t>location,uint16_t health,double movespeed);
 	void Shoot();
 	// Method which player shooting other players
-	/// </summary>
 	void ResetPosition();
 	//Reset position from startPoint
+	// Am facut test
+	// Deoarece dadea eroare la SmarTGIt
+	// Nu se actualiza proiectul in timp real
+	// Si am incercat sa vedem de ce.
+	// Test Test
 private:
 	std::string_view m_name;
 	std::pair<uint16_t, uint16_t>m_location;
