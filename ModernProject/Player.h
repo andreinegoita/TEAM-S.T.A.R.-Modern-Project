1b053c0b61218b59bea547c8da5dc58c811c3212
OnetRares <onetrares@yahoo.com>
2024-10-29 13:23:17 +0200

    Added 2 comments

--- ModernProject/Player.h
+++ ModernProject/Player.h
@@ -7,7 +7,10 @@ class Player
 public:
 	Player( std::string_view name,std::pair<uint16_t, uint16_t>location,uint16_t health,double movespeed);
 	void Shoot();
+	// Method which player shooting other players
+	/// </summary>
 	void ResetPosition();
+	//Reset position from startPoint
 private:
 	std::string_view m_name;
 	std::pair<uint16_t, uint16_t>m_location;
