24b60f567eba3e48a1cd7cf25eb4b28b7c26698a
Oana-Sebastian <sebastian.oana@student.unitbv.ro>
2024-10-30 00:06:58 +0200

    Added getters and modifiers for bullet speed and fire rate, and a method which checks if the weapon can or cannot fire.

--- ModernProject/Weapon.cpp
+++ ModernProject/Weapon.cpp
@@ -3,3 +3,37 @@
 Weapon::Weapon():m_fireRate(4.0),m_bulletSpeed(0.25),m_lastFire(std::chrono::steady_clock::now())
 {
 }
+
+bool Weapon::fire()
+{
+    auto currentTime = std::chrono::steady_clock::now();
+    double timeSinceLastFire = std::chrono::duration<double>(currentTime - m_lastFire).count();
+
+    if (timeSinceLastFire >= m_fireRate) {
+        m_lastFire = currentTime;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void Weapon::upgradeBulletSpeed(double increment)
+{
+    m_bulletSpeed += increment;
+}
+
+void Weapon::reduceFireRate(double factor)
+{
+    m_fireRate = std::max(1.0, m_fireRate - factor);
+}
+
+double Weapon::getFireRate() const
+{
+    return m_fireRate;
+}
+
+double Weapon::getBulletSpeed() const
+{
+    return m_bulletSpeed;
+}
--- ModernProject/Weapon.h
+++ ModernProject/Weapon.h
@@ -2,11 +2,15 @@
 #include<chrono>
 class Weapon
 {
+public:
+	Weapon();
+	bool fire();
+	void upgradeBulletSpeed(double increment);
+	void reduceFireRate(double factor);
+	double getFireRate() const;
+	double getBulletSpeed()const;
 private:
 	double m_fireRate;
 	double m_bulletSpeed;
 	std::chrono::time_point<std::chrono::steady_clock> m_lastFire;
-public:
-	Weapon();
 };
-
