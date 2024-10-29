8334ffef9d61bff458f73591ea96483d8fa158f8
OnetRares <onetrares@yahoo.com>
2024-10-29 13:45:56 +0200

    I added in class Player, method Shoot, Reset Position, and intilizing score and point which count in every match.

--- ModernProject/Player.h
+++ ModernProject/Player.h
@@ -7,20 +7,14 @@ class Player
 public:
 	Player( std::string_view name,std::pair<uint16_t, uint16_t>location,uint16_t health,double movespeed);
 	void Shoot();
-	// Method which player shooting other players
+	// Method which a players shooting another players;
 	void ResetPosition();
-	//Reset position from startPoint
-	// Am facut test
-	// Deoarece dadea eroare la SmarTGIt
-	// Nu se actualiza proiectul in timp real
-	// Si am incercat sa vedem de ce.
-	// Test Test
+	// Method which player respawn in startPoint;
 private:
 	std::string_view m_name;
 	std::pair<uint16_t, uint16_t>m_location;
 	std::uint16_t m_health;
 	double m_movespeed;
-	int points;
-	int score;
-
+	int m_score;
+	int m_points;
 };
