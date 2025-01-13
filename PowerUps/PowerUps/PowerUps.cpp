1428e3faf0d5933176676f573aaca61b0b3a59cd
OnetRares <onetrares@yahoo.com>
2025-01-13 10:23:52 +0200

    Add powerUpTypeString function in dll

--- PowerUps/PowerUps/PowerUps.cpp
+++ PowerUps/PowerUps/PowerUps.cpp
@@ -2,21 +2,21 @@
 #include"PowerUps.h"
 
 
- PowerUpType Power::StringToPowerUpType(const std::string& str)
- {
-     if (str == "SpeedBoost") {
-         return PowerUpType::SpeedBoost;
-     }
-     else if (str == "Shield") {
-         return PowerUpType::Shield;
-     }
-     else if (str == "ExtraLife") {
-         return PowerUpType::ExtraLife;
-     }
-     else if (str == "FireRate") {
-         return PowerUpType::FireRate;
-     }
-     else
-         return PowerUpType::FireRate;
- }
 
+PowerUpType Power::StringToPowerUpType(const std::string& str)
+{
+    if (str == "SpeedBoost") {
+        return PowerUpType::SpeedBoost;
+    }
+    else if (str == "Shield") {
+        return PowerUpType::Shield;
+    }
+    else if (str == "ExtraLife") {
+        return PowerUpType::ExtraLife;
+    }
+    else if (str == "FireRate") {
+        return PowerUpType::FireRate;
+    }
+    else
+        return PowerUpType::FireRate;
+}
