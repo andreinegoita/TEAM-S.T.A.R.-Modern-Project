65cdb9736e8a72a9f106b53900c3316dcb8a7a7f
andreinegoita <lilbabyfan04@gmail.com>
2025-01-18 15:24:58 +0200

    Added the Server class

---
+++ ModernProject/Server.h
@@ -0,0 +1,11 @@
+#pragma once
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#include"GameMap.h"
+#include"Player.h"
+#include "PlayersDatabase.h"
+class Server
+{
+public:
+	void RunServer(GameMap& map, Player& player, http::Storage& storage, std::set<std::pair<int, int>> availableSpawnPositions);
+};
+
